cd1a7eb019561203dfe2e05cdaadb1dc71b546ff
Priya varthini <145207599+Priyavarthini08@users.noreply.github.com>
2023-09-30 23:11:00 +0530

    videoanonymizer.py

--- videoanonymizer.py
+++ videoanonymizer.py
@@ -7,13 +7,12 @@ def process_img(img, face_detection):
 
 
    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-   out = face_detection.process(img_rgb)  # call the face detection process
-
+   out = face_detection.process(img_rgb) 
 
    if out.detections is not None:
-       for detection in out.detections:  # get out img
+       for detection in out.detections:  
            location_data = detection.location_data
-           bbox = location_data.relative_bounding_box  # call bbox
+           bbox = location_data.relative_bounding_box 
            x1, y1, w, h = bbox.xmin, bbox.ymin, bbox.width, bbox.height
            x1 = int(x1 * W)
            y1 = int(y1 * H)
@@ -22,12 +21,12 @@ def process_img(img, face_detection):
 
 
            # blur faces
-           img[y1:y1 + h, x1:x1 + w, :] = cv2.blur(img[y1:y1 + h, x1:x1 + w, :], (30, 30))  # coordinates of the face[]
+           img[y1:y1 + h, x1:x1 + w, :] = cv2.blur(img[y1:y1 + h, x1:x1 + w, :], (30, 30))  
    return img
 
 
 args = argparse.ArgumentParser()
-args.add_argument("--mode", default='video')#we will have argument which user can set in dif mode like video,webcam,image
+args.add_argument("--mode", default='video'
 args.add_argument("--filePath", default='C:\\Users\PRIYA\Downloads\\video.mp4')#specify img path
 args = args.parse_args()
 
