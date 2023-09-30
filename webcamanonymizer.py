6bd03738dee99e6b7272b613b28368c5825bf288
Priya varthini <145207599+Priyavarthini08@users.noreply.github.com>
2023-09-30 23:11:36 +0530

    webcamanonymizer.py

--- webcamanonymizer.py
+++ webcamanonymizer.py
@@ -6,12 +6,12 @@ def process_img(img, face_detection):
     H, W, _ = img.shape
 
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    out = face_detection.process(img_rgb)  # call the face detection process
+    out = face_detection.process(img_rgb)
 
     if out.detections is not None:
-        for detection in out.detections:  # get out img
+        for detection in out.detections: 
             location_data = detection.location_data
-            bbox = location_data.relative_bounding_box  # call bbox
+            bbox = location_data.relative_bounding_box 
             x1, y1, w, h = bbox.xmin, bbox.ymin, bbox.width, bbox.height
             x1 = int(x1 * W)
             y1 = int(y1 * H)
@@ -19,12 +19,12 @@ def process_img(img, face_detection):
             h = int(h * H)
 
             # blur faces
-            img[y1:y1 + h, x1:x1 + w, :] = cv2.blur(img[y1:y1 + h, x1:x1 + w, :], (30, 30))  # coordinates of the face[]
+            img[y1:y1 + h, x1:x1 + w, :] = cv2.blur(img[y1:y1 + h, x1:x1 + w, :], (30, 30)) 
     return img
 
 args = argparse.ArgumentParser()
-args.add_argument("--mode", default='webcam')#we will have argument which user can set in dif mode like video,webcam,image
-args.add_argument("--filePath", default=None)#specify img path
+args.add_argument("--mode", default='webcam')
+args.add_argument("--filePath", default=None)
 args = args.parse_args()
 
 output_dir = 'C:\\Users\PRIYA\Downloads\\output'
